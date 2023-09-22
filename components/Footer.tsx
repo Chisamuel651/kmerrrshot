b770697918b7f6b2dfc305b909d390801cff4278
Chisamuel651 <chijoe0306@gmail.com>
2023-09-22 10:25:24 +0100

    second commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,9 +9,13 @@ type ColumnProps = {
 
 const FooterColumn = ({ title, links }: ColumnProps) => (
   <div className="footer_column">
-    <h4 className="fint-semibold">{title}</h4>
+    <h4 className="font-semibold">{title}</h4>
     <ul className="flex flex-col gap-2 font-normal">
-      {links.map((link) => <Link href='/' key={link}>{link}</Link> )}
+      {links.map((link) => (
+        <Link href="/" key={link}>
+          {link}
+        </Link>
+      ))}
     </ul>
   </div>
 );
@@ -34,8 +38,49 @@ const Footer = () => {
         </div>
 
         <div className="flex flex-wrap gap-12">
-          <FooterColumn title={footerLinks[0].title} links={footerLinks[0].links} />
+          <FooterColumn
+            title={footerLinks[0].title}
+            links={footerLinks[0].links}
+          />
+          <div className="flex-1 flex flex-col gap-4">
+            <FooterColumn
+              title={footerLinks[1].title}
+              links={footerLinks[1].links}
+            />
+            <FooterColumn
+              title={footerLinks[2].title}
+              links={footerLinks[2].links}
+            />
+          </div>
+
+          <FooterColumn
+            title={footerLinks[3].title}
+            links={footerLinks[3].links}
+          />
+
+          <div className="flex-1 flex flex-col gap-4">
+            <FooterColumn
+              title={footerLinks[4].title}
+              links={footerLinks[4].links}
+            />
+            <FooterColumn
+              title={footerLinks[5].title}
+              links={footerLinks[5].links}
+            />
+          </div>
+
+          <FooterColumn
+            title={footerLinks[6].title}
+            links={footerLinks[6].links}
+          />
         </div>
+      </div>
+
+      <div className="flexBetween footer_copyright">
+        <p>@ 2023 Kmerrrshot. All Rights Reserved</p>
+        <p className="text-gray">
+          <span className="text-black font-semibold">2300</span> Projects Submitted
+        </p>
       </div>
     </footer>
   );
